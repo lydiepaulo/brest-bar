48e20a038b6f512a6bcea4d31e0c1a182ce0aac0
Lydie Paulo <lydiepaulo.ui@gmail.com>
2023-11-26 08:29:13 +0100

    adjustment colors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5dbdf6bfacf6ca0a"; }
+/******/ 		__webpack_require__.h = function() { return "a3181e8b4a0d7c41"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
